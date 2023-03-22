3f203c2797cb5e29409a543042a34fb41fecb698
Alex <128611132+AlexFromXbash@users.noreply.github.com>
2023-03-22 15:48:59 +0100

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,13 @@
+pip install base64
+pip install ctypes
+pip install json
+pip install msvcrt
+pip install random
+pip install colorama
+pip install os
+pip install discord
+pip install requests
+pip install time
+pip install getpass
+pip install discord.py
+python loader.py
